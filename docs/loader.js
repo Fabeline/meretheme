1d2743d7601a13ebd8ef1e6fef2cec46a9361c02
Merethe Granevang <fabeline@hotmail.com>
2024-04-21 21:07:59 +0200

    Added loader

---
+++ docs/loader.js
@@ -0,0 +1,13 @@
+const merethemeStyleCDN = "https://cdn.jsdelivr.net/gh/fabeline/meretheme@latest/lib/meretheme.js"; //"file:///C:/progging/meretheme/lib/meretheme.css";
+const merethemeCodeCDN =  "https://cdn.jsdelivr.net/gh/fabeline/meretheme@latest/lib/meretheme.js"; //"file:///C:/progging/meretheme/lib/meretheme.js";
+
+window.addEventListener('load', function() {
+    var script = document.createElement('script');
+    script.src = merethemeCodeCDN;
+    document.head.appendChild(script);
+
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = merethemeStyleCDN;
+    document.head.appendChild(link);
+});
