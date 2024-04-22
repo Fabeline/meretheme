40e9155352010957082fe339534559c67381ff46
Merethe Granevang <fabeline@hotmail.com>
2024-04-22 19:13:38 +0200

    Updated opacity on load

--- docs/loader.js
+++ docs/loader.js
@@ -2,8 +2,7 @@ const merethemeStyleCDN = "https://cdn.jsdelivr.net/gh/fabeline/meretheme@latest
 const merethemeCodeCDN =  "https://cdn.jsdelivr.net/gh/fabeline/meretheme@latest/lib/meretheme.js"; //"file:///C:/progging/meretheme/lib/meretheme.js";
 
 window.addEventListener('load', function() {
-    //document.body.style.opacity = 0;
-
+    document.body.style.opacity = 0;
     var script = document.createElement('script');
     script.src = merethemeCodeCDN;
     document.head.appendChild(script);
